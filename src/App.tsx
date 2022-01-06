cd01a26feaa252c562c2ec880ee0e3592b095546
João Pedro Mota Raffo Rodrigues <joao.raffo@gft.com>
2022-01-06 09:47:07 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+// import { Container } from './styles';
+
+const src: React.FC = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text style={{fontSize: 72}}>PokéPedia</Text>
+    </View>
+  );
+};
+
+export default src;
